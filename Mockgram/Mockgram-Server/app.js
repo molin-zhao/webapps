c48d4363fc3b9f11162942a5c15254067d502d55
molin <molinz@molins-MacBook-Pro.local>
2018-09-21 01:28:16 +1000

    static path

--- Mockgram/Mockgram-Server/app.js
+++ Mockgram/Mockgram-Server/app.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/mockgram', express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/mockgram', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
