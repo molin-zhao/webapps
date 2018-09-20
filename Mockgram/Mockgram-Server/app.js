96706102488ccd8bcc0a3207bb68786b2f7a2462
molin <molinz@molins-MacBook-Pro.local>
2018-09-21 01:35:41 +1000

    change path

--- Mockgram/Mockgram-Server/app.js
+++ Mockgram/Mockgram-Server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use('/mockgram', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/mockgram', indexRouter);
 
