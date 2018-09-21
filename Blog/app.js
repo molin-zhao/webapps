acf6405c53bc6a6aba4f4c0813a7116bfb92c207
molin <molinz@molins-MacBook-Pro.local>
2018-09-21 20:07:35 +1000

    blog error handler, redirect on 404

--- Blog/app.js
+++ Blog/app.js
@@ -17,9 +17,23 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'hexo/public')));
 
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // redirect to index page
+    res.redirect('/');
+});
 
 module.exports = app;
