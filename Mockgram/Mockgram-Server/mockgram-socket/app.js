96ac475460181fa76df12196204d403365221c43
molin <molinz@molins-MacBook-Pro.local>
2018-09-27 19:47:05 +1000

    socket.io

--- Mockgram/Mockgram-Server/mockgram-socket/app.js
+++ Mockgram/Mockgram-Server/mockgram-socket/app.js
@@ -3,9 +3,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const redis = require('redis');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const indexRouter = require('./routes/index');
 const messageRouter = require('./routes/message-push');
+const config = require('../config');
+const authenticate = require('../mockgram-utils/utils/authenticate');
 
 const app = express();
 
@@ -14,13 +21,52 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+
+// set up mongoose connection
+let mongoUrl = `mongodb://${config.mongoUrl.host}:${config.mongoUrl.port}/${config.mongoUrl.db}`;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('connected correctly to mongodb');
+}).catch(err => console.log(err));
+
+// set up session information
+// create session middleware for both express and socket.io
+const sessionMiddleWare = session({
+  store: new RedisStore({
+    client: redis.createClient(config.redisUrl.port, config.redisUrl.host)
+  }),
+  secret: config.secretKey,
+  resave: false,
+  saveUninitialized: false,
+  name: config.name,
+  cookie: config.cookie
+});
+// apply middleware to app
+app.use(sessionMiddleWare);
+
+// set up passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+
+// adding a generic JSON and URL-encoded parser as a top-level middleware, 
+// which will parse the bodies of all incoming requests.
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// parse application/json
+app.use(bodyParser.json());
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set up routers
 app.use('/', indexRouter);
 app.use('/message', messageRouter);
 
@@ -40,4 +86,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = {
+  app: app,
+  sessionMiddleWare: sessionMiddleWare
+};
