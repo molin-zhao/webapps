b22dbbb05b9aa2de81da6d6de34e830e688e4807
molin <molinz@ua-unistudent-ten-9-130-69.uniaccess.unimelb.edu.au>
2019-06-26 10:58:27 +0800

    edit bash

--- Mockgram/Mockgram-Server/start.sh
+++ Mockgram/Mockgram-Server/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd /mockgram-api
+cd mockgram-api
 pm2 start npm --name mockgram_api -- start
 cd ..
-cd /mockgram-image
+cd mockgram-image
 pm2 start npm --name mockgram_image -- start
 cd ..
-cd /mockgram-socket 
+cd mockgram-socket 
 pm2 start npm --name mockgram_socket -- start
 cd ..
-cd /mockgram-ml
+cd mockgram-ml
 pm2 start app.py --name mockgram_ml --interpreter=python3
 cd ..
-cd /mockgram-web
+cd mockgram-web
 pm2 start npm --name mockgram_web -- start
 exit 0
